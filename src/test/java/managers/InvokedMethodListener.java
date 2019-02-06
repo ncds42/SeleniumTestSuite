166852f9af82022114a306c45e0d8ae7a7ce19f0
ncds42 <ncds42@gmail.com>
2019-02-06 03:11:53 -0600

    made picture folder

--- src/test/java/managers/InvokedMethodListener.java
+++ src/test/java/managers/InvokedMethodListener.java
@@ -43,7 +43,7 @@ public class InvokedMethodListener implements IInvokedMethodListener {
 
 				File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
 				try {
-					String dest = "/home/ubuntu/testSuitePictures/";
+					String dest = "testSuitePictures/";
 					String fileName = new Timestamp(System.currentTimeMillis()).toString();
 					fileName = fileName.replaceAll("-","");
 					fileName = fileName.replaceAll(" ","");
